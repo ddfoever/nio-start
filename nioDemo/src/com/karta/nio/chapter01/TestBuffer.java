8d06841e87efd287fd41b59471c5626b66b0e9bf
IT_An <an_ning914@163.com>
2019-08-06 23:36:57 +0800

    Day1:    1. allocate data type buffer    2. put data into buffer    3. handler data in buffer

---
+++ nioDemo/src/com/karta/nio/chapter01/TestBuffer.java
@@ -0,0 +1,12 @@
+package com.karta.nio.chapter01;
+
+/**
+ *一.缓冲区Buffer, 在Java NIO中负责存储数据，而缓冲区其本质就是一个数组，用于存储不同数据类型的数据
+ *
+ */
+public class TestBuffer {
+    public static void main(String[] args) {
+
+    }
+
+}
